178df31d8192b6bc2281c7e83630d6b0d0e38d9c
nguyenthichi <chinguyen270296@gmail.com>
2018-12-05 21:26:19 +0700

    TD 5_ ghép

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 import Signin from './signin/Signin';
 import HomePage from './homepage/HomePage';
 import Guide from './guide/Guide';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <Signin/> */}
-        <Guide/>
-      </div>
+      // <div className="App">
+      //   {/* <Signin/> */}
+      //   <Guide/>
+      // </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={() => <Header />} />
+            <Route exact path="/guide" component={() => <Guide />} />
+            <Route exact path="/sign_in" component={() => <Signin />} />
+          </Switch>
+
+        </div>
+      </Router>
     );
   }
 }
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router'
+import { reactLocalStorage } from 'reactjs-localstorage';
 
 import './header.css';
 import im_step1 from '../images/step1.png';
@@ -12,6 +14,8 @@ class Header extends Component {
             hoverSupport: false,
             hoverLogin: false,
             hoverSignIn: false,
+            redirectGuide: false,
+            redirectSignIn: false,
         }
     }
 
@@ -51,74 +55,74 @@ class Header extends Component {
         })
     };
 
+    redirectGuide = () => {
+        this.setState({ redirectGuide: true });
+    }
+
+    redirectSignIn = () => {
+        this.setState({ redirectSignIn: true });
+    }
+
     render() {
+        if (this.state.redirectGuide) {
+            return <Redirect push to="/guide" />;
+        }
+        if (this.state.redirectSignIn) {
+            return <Redirect push to="/sign_in" />;
+        }
         return (
             <div>
-                {/* <div className="container">
-                    <div className="logo"> logo</div>
-                    <div className="navigation">
-                        <div className="button">
-
-                        </div>
-                        <div className="main-menu">
-                            <ul>
-                                <li>
-                                    <a>
-                                        <div className="item_menu" style={{ color: (this.state.hoverSupport ? "#006400" : "#fff") }} onMouseEnter={this.handleSupportHoverOn} onMouseLeave={this.handleSupportHoverOff}>Hỗ trợ</div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a>
-                                        <div className="item_menu" style={{ color: (this.state.hoverLogin ? "#006400" : "#fff") }} onMouseEnter={this.handleLoginHoverOn} onMouseLeave={this.handleLoginHoverOff}>Đăng nhập</div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a>
-                                        <div className="item_menu" style={{ color: (this.state.hoverSignIn ? "#006400" : "#fff") }} onMouseEnter={this.handleSignInHoverOn} onMouseLeave={this.handleSignInHoverOff}>Đăng ký</div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div> */}
                 <header id="header">
                     <nav className="navbar navbar-dark bg-primary header">
                         <div className="nav-ul">
                             <label className="nav-a">LOGO</label>
-                            <a className="nav-a"><i className="zmdi zmdi-phone"></i> 0965143540</a>
-                            <a className="nav-a"><i className="zmdi zmdi-email"></i>ducdt@gmail.com</a>
-                            <a className="nav-a"><i className="zmdi zmdi-facebook"></i> Facebook</a>
-                            <a className="nav-a"><i className="zmdi zmdi-info-outline"></i> Hướng dẫn</a>
-                            <a className="nav-a">Đăng nhập</a>
-                            <a className="nav-a nav-dangky">Đăng ký</a>
+                            <span className="nav-a"><i className="zmdi zmdi-phone"></i> 0965143540</span>
+                            <span className="nav-a"><i className="zmdi zmdi-email"></i>ducdt@gmail.com</span>
+                            <span className="nav-a"><i className="zmdi zmdi-facebook"></i> Facebook</span>
+                            <span className="nav-a"
+                                style={{ color: (this.state.hoverSupport ? "#fff" : "#000") }}
+                                onMouseEnter={this.handleSupportHoverOn}
+                                onMouseLeave={this.handleSupportHoverOff}
+                                onClick={this.redirectGuide} >
+                                <i className="zmdi zmdi-info-outline">
+                                </i> Hướng dẫn
+                            </span>
+                            <span className="nav-a"
+                                style={{ color: (this.state.hoverLogin ? "#fff" : "#000") }}
+                                onMouseEnter={this.handleLoginHoverOn} onMouseLeave={this.handleLoginHoverOff}>Đăng nhập
+                                </span>
+                            <span className="nav-a nav-dangky"
+                                style={{ color: (this.state.hoverSignIn ? "#fff" : "#000") }}
+                                onMouseEnter={this.handleSignInHoverOn}
+                                onMouseLeave={this.handleSignInHoverOff}
+                                onClick={this.redirectSignIn}>Đăng ký</span>
                         </div>
-
                     </nav>
                 </header>
                 <div className="con-search">
                     <div className="search">
-                        <label className="text-search"> Thuê xe tự lái</label>
+                        <label className="text-search"> Thuê xe tự lái - Tải ngay OcChoOTo</label>
                         <div className="input-search">
-                             <input type="text" className="" id="address" placeholder="Nhập địa chỉ ..."/>
+                            <input type="text" className="" id="address" placeholder="Nhập địa chỉ ..." />
                         </div>
                     </div>
                 </div>
                 <div className="guide">
                     <h1>Hướng dẫn thuê xe </h1>
                     <div className="con-guide">
-                        <div className="step1">
+                        <div className="step1" onClick={this.redirectGuide}>
                             <img src={im_step1} />
                             <h3>Đặt xe</h3>
                         </div>
-                        <div className="step2">
+                        <div className="step2" onClick={this.redirectGuide}>
                             <img src={im_step2} />
                             <h3>Nhận xe hoặc giao tận nơi</h3>
                         </div>
-                        <div className="step3">
-                            <img src={im_step3}/>
+                        <div className="step3" onClick={this.redirectGuide}>
+                            <img src={im_step3} />
                             <h3>Trải nghiệm chuyến đi</h3>
                         </div>
-                        <div className="step4">
+                        <div className="step4" onClick={this.redirectGuide}>
                             <img src={im_step4} />
                             <h3>Kết thúc giao dịch</h3>
                         </div>
