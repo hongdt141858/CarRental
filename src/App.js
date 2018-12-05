9fe6dd075a00dc030f0c7022141c291205b4cc6b
nguyenthichi <chinguyen270296@gmail.com>
2018-12-05 16:41:44 +0700

    TD 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Header from './header/Header';
 import './App.css';
 import Signin from './signin/Signin';
 
@@ -7,7 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Signin/>
+        {/* <Signin /> */}
+        <Header />
       </div>
     );
   }
