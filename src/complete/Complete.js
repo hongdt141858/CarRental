c01b501cb2a05ce95491822ceda1fd966792d378
DoTriDucBK <20141104@student.hust.edu.vn>
2018-12-13 21:38:35 +0700

    edit customer

---
+++ src/complete/Complete.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './complete.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class Complete extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
